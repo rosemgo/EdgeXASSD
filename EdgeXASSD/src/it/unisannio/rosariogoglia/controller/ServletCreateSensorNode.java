aa34ce9b0edebb7ecb7814a9e3660abd0e1bf76a
rosar <rosariogoglia@gmail.com>
2022-07-23 12:22:00 +0200

    Inserimento componente server per il nodo sensore con protocollo COAP.

--- EdgeXASSD/src/it/unisannio/rosariogoglia/controller/ServletCreateSensorNode.java
+++ EdgeXASSD/src/it/unisannio/rosariogoglia/controller/ServletCreateSensorNode.java
@@ -12,6 +12,7 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 import java.util.Date;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -47,88 +48,145 @@ public class ServletCreateSensorNode extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String messaggio = "";
+		Integer sensorNodeProtocol = null;
+		SensorNodeDAO snDAO = new SensorNodeDAO();
+		ProtocolDAO pDAO = new ProtocolDAO();
+		
 		
 		//RICEVERE I PARAMENTRI DEL NODO SENSORE DA CREARE
 		String sensorNodeName = request.getParameter("device");
-		Integer sensorNodeProtocol = Integer.parseInt(request.getParameter("idProtocol")); //farlo scegliere tra una lista ajax di protocolli
 		
-		//EFFETTUARE CONTROLLO SUI VALORI INSERIRI
+		String protocolSN = request.getParameter("idProtocol"); //farlo scegliere tra una lista ajax di protocolli
+		if(!protocolSN.equals("Select Protocol")) {
+			sensorNodeProtocol = Integer.parseInt(protocolSN);
+		} 
+				
 		
-		SensorNodeDAO snDAO = new SensorNodeDAO();
-		ProtocolDAO pDAO = new ProtocolDAO();
-		Protocol protocol = pDAO.getProtocolById(sensorNodeProtocol);
+		if(sensorNodeName.equals("")){
+			
+			messaggio = "Devi inserire un nome valido!";
+			response.setContentType("text/plain");
+	        response.setCharacterEncoding("UTF-8");
+	        response.getWriter().write(messaggio);
+			
+		}else if(sensorNodeProtocol==null) {
+			
+			messaggio = "Devi scegliere un protocollo di comunicazione!";
+			response.setContentType("text/plain");
+	        response.setCharacterEncoding("UTF-8");
+	        response.getWriter().write(messaggio);
 		
-		SensorNode sensorNode = null;
-		
-		if (protocol.getProtocol().equals("MQTT")){
-			sensorNode = new SensorNodeMQTT();
 		}
-		if (protocol.getProtocol().equals("COAP")) {
-			sensorNode = new SensorNodeCOAP(); 
-		}
-		if (protocol.getProtocol().equals("REST")) {
-			sensorNode = new SensorNodeREST();
-		}	
-		
-		sensorNode.setDevice(sensorNodeName);
-		sensorNode.setProtocollo(protocol); //sensorNodeProtocol DEVE ESSERE SCRITTO IN STAMPATELLO
-		int idSensorNode = -1;
-		//SCRIVERE NEL DATABASE I DATI DEL NODO SENSORE CREATO
-		idSensorNode = snDAO.insertSensorNode(sensorNode);
-		
-		if(idSensorNode!=-1) {
-			messaggio = "Nodo Sesnore n° "+ sensorNode.getIdSensorNode() + " " + sensorNode.getDevice() + " creato correttamente!";
-			logger.info(new Date()+" "+messaggio);
-			
-			String deviceEdgeX = "";
-		
-			//DIFFERENZIARE LE STRINGHE DA CREARE IN BASE AL TIPO DI PROTOCOLLO SCELTO
-			//IL NOME SCELTO DALL'UTENTE SARà IL DEVICENAME NEL DATABASE E IN EDGEX
-			
-			if(sensorNode.getProtocollo().getProtocol().equals("MQTT")) {
-				deviceEdgeX = "[{\"apiVersion\": \"v2\",\"device\": {\"name\": \""+ sensorNode.getDevice() +"\",\"description\": \"Sensor Node MQTT creato dall'utente in data: "+new Date()+"\",\"adminState\": \"UNLOCKED\",\"operatingState\": \"UP\",\"labels\": [\"mqtt\",\"utente\"],\"serviceName\": \"device-mqtt\",\"profileName\": \"Test-Device-MQTT-Profile\",\"protocols\": {\"mqtt\": {\"CommandTopic\": \"command/"+sensorNode.getDevice()+"\"}}}}]";
-			}
-			else if(sensorNode.getProtocollo().getProtocol().equals("COAP")){
-				deviceEdgeX = "[{\"apiVersion\": \"v2\",\"device\": {\"name\": \""+ sensorNode.getDevice() +"\",\"description\": \"Sensor Node COAP creato dall'utente in data: "+new Date()+"\",\"adminState\": \"UNLOCKED\",\"operatingState\": \"UP\",\"labels\": [\"coap\",\"utente\"],\"serviceName\": \"device-coap\",\"profileName\": \"example-datatype\",\"protocols\": {\"COAP\": {\"ED_ADDR\": \"192.168.138.252\",\"ED_SecurityMode\": \"NoSec\"}}}}]";
-			}
-			else if(sensorNode.getProtocollo().getProtocol().equals("REST")){
-				deviceEdgeX = "[{\"apiVersion\": \"v2\",\"device\": {\"name\": \""+ sensorNode.getDevice() +"\",\"description\": \"Sensor Node REST creato dall'utente in data: "+new Date()+ "\",\"adminState\": \"UNLOCKED\",\"operatingState\": \"UP\",\"labels\": [\"rest\",\"utente\"],\"serviceName\": \"device-rest\",\"profileName\": \"sample-json\",\"protocols\": {\"other\": {}}}}]";
-			}
-		
-			//ESEGUIRE UNA RICHIESTA POST PER CARICARE IL DEVICE IN EDGEX
-			
-			HttpClient client = HttpClient.newBuilder().build();
-			
-			HttpRequest requestEdgeX = HttpRequest.newBuilder()  
-			        .uri(URI.create("http://192.168.204.133:59881/api/v2/device")) //invio una richiesta REST al servizio metadata per aggiungere un device ad EdgeX
-			        .timeout(Duration.ofMinutes(2))
-			        .header("Content-Type", "application/json")
-			        .POST(BodyPublishers.ofString( deviceEdgeX )) //BodyPublisher converte un oggetto java di alto livello in un flusso di dati
-			        .build();
-			
-			try {
-				HttpResponse<String> responseEdgeX = client.send(requestEdgeX, BodyHandlers.ofString());  //E' il duale di BodyPublisher, converte un fusso di dati in un oggetto java di alto livello
-				out.printf("Response code is: %d %n", responseEdgeX.statusCode());
-				out.printf("The response body is:%n %s %n", responseEdgeX.body());			
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			
-			
+		else if(snDAO.checkSensorNodeByNome(sensorNodeName)){ //VERIFICARE SE ESISTE GIà UN DEVICE CON LO STESSO NOME
+			messaggio = "Già esiste un nodo sensore con lo stesso nome!";
+			response.setContentType("text/plain");
+	        response.setCharacterEncoding("UTF-8");
+	        response.getWriter().write(messaggio);
 		}
 		else {
-			messaggio="Nodo Sensore "+ sensorNodeName +" non creato correttamente!!!";
-			logger.info(new Date()+" "+messaggio);	
 			
-		}
-				
+		//	SensorNodeDAO snDAO = new SensorNodeDAO();
+		//	ProtocolDAO pDAO = new ProtocolDAO();
+			
 
-		request.setAttribute("messaggio", messaggio);
-		request.getRequestDispatcher("dashboard.jsp").forward(request, response);
-		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+		//CREARE UN METODO DAO IN SENSORNODE CHE FORNISCE IL NUMERO DI PORTA PIù GRANDE PRESENTE NEL DB
+		// SE NON C'è NESSUN NODO COAP INIZIARE CON UN NUMERO FISSO
+			
+
+			
+			Protocol protocol = pDAO.getProtocolById(sensorNodeProtocol);
+			
+			SensorNode sensorNode = null;
+			
+			if (protocol.getProtocol().equals("MQTT")){
+				sensorNode = new SensorNodeMQTT();
+			}
+			if (protocol.getProtocol().equals("COAP")) { 
+				//PASSARE LA PORTA DELLA COMPONENTE SERVER SEMPRE CRESCENTE, AD OGNI NODO CHE HA IL PROTOCOLLO COAP
+				Integer maxPort = snDAO.getMaxPortServerCOAP(); //ottengo il valore di porta più grande associato all'ultimo nodo sensore COAP inserito nel DB
+				Integer port;
+				if(maxPort == null)
+					port = 45000;
+				else				
+					port = maxPort + 1; //creiamo una porta di uno più grande della massima presente 
+				sensorNode = new SensorNodeCOAP(port); 
+			}
+			if (protocol.getProtocol().equals("REST")) {
+				sensorNode = new SensorNodeREST();
+			}	
+			
+			sensorNode.setDevice(sensorNodeName);
+			sensorNode.setProtocollo(protocol);
+			
+			
+			int idSensorNode = -1;
+			//SCRIVERE NEL DATABASE I DATI DEL NODO SENSORE CREATO
+			idSensorNode = snDAO.insertSensorNode(sensorNode);
+			
+			if(idSensorNode!=-1) {
+				messaggio = "Nodo Sensore n° "+ sensorNode.getIdSensorNode() + " " + sensorNode.getDevice() + " creato correttamente!";
+				logger.info(new Date()+" "+messaggio);
+				
+				String deviceEdgeX = "";
+			
+				//DIFFERENZIARE LE STRINGHE DA CREARE IN BASE AL TIPO DI PROTOCOLLO SCELTO
+				//IL NOME SCELTO DALL'UTENTE SARà IL DEVICENAME NEL DATABASE ED IN EDGEX
+				
+				if(sensorNode.getProtocollo().getProtocol().equals("MQTT")) {
+					deviceEdgeX = "[{\"apiVersion\": \"v2\",\"device\": {\"name\": \""+ sensorNode.getDevice() +"\",\"description\": \"Sensor Node MQTT creato dall'utente in data: "+new Date()+"\",\"adminState\": \"UNLOCKED\",\"operatingState\": \"UP\",\"labels\": [\"mqtt\",\"utente\"],\"serviceName\": \"device-mqtt\",\"profileName\": \"Test-Device-MQTT-Profile\",\"protocols\": {\"mqtt\": {\"CommandTopic\": \"command/" + sensorNode.getDevice() + "\"}}}}]";
+				}
+				else if(sensorNode.getProtocollo().getProtocol().equals("COAP")){
+					deviceEdgeX = "[{\"apiVersion\": \"v2\",\"device\": {\"name\": \""+ sensorNode.getDevice() +"\",\"description\": \"Sensor Node COAP creato dall'utente in data: "+new Date()+"\",\"adminState\": \"UNLOCKED\",\"operatingState\": \"UP\",\"labels\": [\"coap\",\"utente\"],\"serviceName\": \"device-coap\",\"profileName\": \"example-datatype\",\"protocols\": {\"COAP\": {\"ED_ADDR\": \"192.168.138.252\", \"ED_PORT\": \""+ ((SensorNodeCOAP) sensorNode).getPort() +"\",\"ED_SecurityMode\": \"NoSec\"}}}}]";
+				}
+				else if(sensorNode.getProtocollo().getProtocol().equals("REST")){
+					deviceEdgeX = "[{\"apiVersion\": \"v2\",\"device\": {\"name\": \""+ sensorNode.getDevice() +"\",\"description\": \"Sensor Node REST creato dall'utente in data: "+new Date()+ "\",\"adminState\": \"UNLOCKED\",\"operatingState\": \"UP\",\"labels\": [\"rest\",\"utente\"],\"serviceName\": \"device-rest\",\"profileName\": \"sample-json\",\"protocols\": {\"other\": {}}}}]";
+				}
+			
+				//ESEGUIRE UNA RICHIESTA POST PER CARICARE IL DEVICE IN EDGEX
+				
+				HttpClient client = HttpClient.newBuilder().build();
+				
+				HttpRequest requestEdgeX = HttpRequest.newBuilder()  
+				        .uri(URI.create("http://192.168.204.133:59881/api/v2/device")) //invio una richiesta REST al servizio metadata per aggiungere un device ad EdgeX
+				        .timeout(Duration.ofMinutes(2))
+				        .header("Content-Type", "application/json")
+				        .POST(BodyPublishers.ofString( deviceEdgeX )) //BodyPublisher converte un oggetto java di alto livello in un flusso di dati
+				        .build();
+				
+				try {
+					HttpResponse<String> responseEdgeX = client.send(requestEdgeX, BodyHandlers.ofString());  //E' il duale di BodyPublisher, converte un fusso di dati in un oggetto java di alto livello
+					out.printf("Response code is: %d %n", responseEdgeX.statusCode());
+					out.printf("The response body is:%n %s %n", responseEdgeX.body());			
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				
+				
+			}
+			else {
+				messaggio="Nodo Sensore "+ sensorNodeName +" non creato correttamente!!!";
+				logger.info(new Date()+" "+messaggio);	
+				
+			}
+					
+			
+		//	request.setAttribute("messaggio", messaggio);
+		//	request.getRequestDispatcher("dashboard.html").forward(request, response);
+			
+			
+			
+			response.setContentType("text/plain");
+	        response.setCharacterEncoding("UTF-8");
+	        response.getWriter().write(messaggio);
+			
+			
+			
+			
+			//response.getWriter().append("Served at: ").append(request.getContextPath());
+						
+		}
+			
 	}
 
 	/**
--- EdgeXASSD/src/it/unisannio/rosariogoglia/model/SensorNodeCOAP.java
+++ EdgeXASSD/src/it/unisannio/rosariogoglia/model/SensorNodeCOAP.java
@@ -8,93 +8,210 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import it.unipr.netsec.mjcoap.coap.client.CoapClient;
+import it.unipr.netsec.mjcoap.coap.message.CoapMessageFactory;
+import it.unipr.netsec.mjcoap.coap.message.CoapRequest;
 import it.unipr.netsec.mjcoap.coap.message.CoapRequestMethod;
 import it.unipr.netsec.mjcoap.coap.message.CoapResponse;
+import it.unipr.netsec.mjcoap.coap.message.CoapResponseCode;
 import it.unipr.netsec.mjcoap.coap.provider.CoapURI;
+import it.unipr.netsec.mjcoap.coap.server.AbstractCoapServer;
 import it.unipr.netsec.mjcoap.coap.server.CoapResource;
 
 public class SensorNodeCOAP extends SensorNode{
 
+	private int port; //porta di ascolto della componente server del nodo sensore COAP, utilizzata dal device COAP per comunicare col server.
+	
+	public SensorNodeCOAP(int port) {
+		super();
+		this.port = port;
+	}
+	
+	
+	public SensorNodeCOAP() {}
 
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+
+
+	public class ClientCOAP extends Thread{
+		
+		public void run() {
+			
+			System.out.println("INIZIO RUN IN CLIENT COAP");
+			
+			//args.length > 0 ? args[0] : 
+			CoapURI resource_uri;
+			
+			try {
+			//	resource_uri = new CoapURI("coap://192.168.204.133/a1r/d2/json"); //IN EDGEX DEVO AVERE UN DEVICE PER OGNI NODO SENSORE
+				resource_uri = new CoapURI("coap://192.168.204.133/a1r/"+ SensorNodeCOAP.this.device +"/json");  //this.device DEVE CORRISPONDERE AL NOME DEL DISPOSITIVO CONTENUTO IN EDGEX	
+				int udp_port=5683;
+				CoapClient coap_client = new CoapClient(udp_port);
+				
+				System.out.println("CONNESSO AL SERVER COAP: " + resource_uri + " " + coap_client.toString());
+				
+				//INVIO ASYNC EVENT				
+				
+				long time1 = System.currentTimeMillis();		
+				long time2 = 0;
+				//String resource = "json";
+				
+				while(true) {
+				
+					time2 = System.currentTimeMillis();
+				    
+				    if(time2 - time1 > 10000) {
+						//ogni sensore associato al nodo sensore invia una misurazione
+						for(int i=0; i<SensorNodeCOAP.this.sensors.size(); i++) {
+							
+							//CREARE UN MESSAGGIO JSON PER OGNI SENSORE ED EFFETTUARE L'INVIO
+							JSONObject jsonmsg = new JSONObject();
+					    	jsonmsg.put("nameNode", SensorNodeCOAP.this.device); //nome del sensore appartenente al nodo sensore
+					    	jsonmsg.put("nameSensor", SensorNodeCOAP.this.sensors.get(i).getName()); 
+					    	jsonmsg.put("type", SensorNodeCOAP.this.sensors.get(i).getType()); 
+					    	jsonmsg.put("value", SensorNodeCOAP.this.sensors.get(i).measurement());
+					    	jsonmsg.put("data", new Date());
+					    				    	
+						    System.out.println("MESSAGGIO INVIATO: " + jsonmsg);
+							CoapResponse resp=coap_client.request(CoapRequestMethod.POST, resource_uri, CoapResource.FORMAT_TEXT_PLAIN_UTF8, jsonmsg.toString().getBytes());
+								
+						//	resp = coap_client.request(CoapRequestMethod.POST, resource_uri, 0, "751".getBytes());
+							
+							
+							if (resp!=null) 
+								System.out.println("Response: "+resp);
+							else  
+								System.out.println("Request failure");
+							
+							
+						}
+					
+						time1 = System.currentTimeMillis();
+				    }
+				
+				}
+				
+			//	coap_client.halt();
+
+			//	System.out.println("FINE DEL RUN");
+		
+		
+			
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	/**
+	 * Componente server del nodo sensore COAP, utilizzata per ricevere i comandi dal device in EdgeX
+	 * @author rosar
+	 *
+	 */
+	public class ServerCOAP extends Thread{
+			
+		public void run(){
+			
+			System.out.println("SERVER STARTED");
+			
+			try {
+				//il server è in ascolto sulla porta specifica passata al nodo sensore
+				new AbstractCoapServer(SensorNodeCOAP.this.port) {
+								
+					@Override
+					protected void handleGetRequest(CoapRequest req) {
+										
+						//System.out.println(" ");
+						//System.out.println("SERVER STARTED");
+						
+						System.out.println("messageID"+req.getMessageId());
+						System.out.println("tipologia di mess: " + req.getType()); 
+							
+						try {
+								System.out.println("uri"+req.getRequestUri());
+								System.out.println("uri PATH:" + req.getRequestUriPath());
+						} catch (URISyntaxException e) {
+								e.printStackTrace();
+						}
+						System.out.println("method "+req.getMethod());
+						System.out.println("Payload "+req.getPayload());
+							
+							
+							if(req.getRequestUriPath().contains("json")) {
+								
+								JSONObject jsonmsg = new JSONObject();
+						    	try {
+									jsonmsg.put("nameNode", "PIPPO");
+									jsonmsg.put("nameSensor", "PLUTO");
+									jsonmsg.put("data", new Date());
+								} catch (JSONException e) {
+									e.printStackTrace();
+								}
+						    	
+						    	//CoapResponse resp = coap_client.request(CoapRequestMethod.POST, resource_uri, 0, "739".getBytes());
+												    	
+						    	CoapResponse resp = CoapMessageFactory.createResponse(req, CoapResponseCode._2_05_Content);
+								resp.setPayload(CoapResource.FORMAT_TEXT_PLAIN_UTF8, jsonmsg.toString().getBytes());
+								respond(req,resp);
+								System.out.println("RISPOSTA: " + resp.getPayload().toString());
+								System.out.println(resp.getRemoteSoAddress());
+							}
+							else{
+								CoapResponse resp = CoapMessageFactory.createResponse(req,CoapResponseCode._2_05_Content);
+								resp.setPayload(CoapResource.FORMAT_TEXT_PLAIN_UTF8,"1".getBytes());
+								respond(req,resp);	
+							}
+						
+					}
+				};
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
 	
 	@Override
 	public void run() {
 		
-		System.out.println("INIZIO RUN");
+		//far partire un thread client coap ed uno server coap
 		
-		//args.length > 0 ? args[0] : 
-		CoapURI resource_uri;
-		
-		try {
-		//	resource_uri = new CoapURI("coap://192.168.204.133/a1r/d2/json"); //IN EDGEX DEVO AVERE UN DEVICE PER OGNI NODO SENSORE
-			resource_uri = new CoapURI("coap://192.168.204.133/a1r/"+ this.device +"/json");  //this.device DEVE CORRISPONDERE AL NOME DEL DISPOSITIVO CONTENUTO IN EDGEX	
-			int udp_port=5683;
-			CoapClient coap_client = new CoapClient(udp_port);
-			
-			System.out.println("CONNESSO AL SERVER COAP: " + resource_uri + " " + coap_client.toString());
-			
-			//INVIO ASYNC EVENT				
-			
-			long time1 = System.currentTimeMillis();		
-			long time2 = 0;
-			//String resource = "json";
-			
-			while(true) {
-			
-				time2 = System.currentTimeMillis();
-			    
-			    if(time2 - time1 > 10000) {
-					//ogni sensore associato al nodo sensore invia una misurazione
-					for(int i=0; i<this.sensors.size(); i++) {
-						
-						//CREARE UN MESSAGGIO JSON PER OGNI SENSORE ED EFFETTUARE L'INVIO
-						      		
-		            
-		            	JSONObject jsonmsg = new JSONObject();
-				    	jsonmsg.put("nameNode", this.device); //nome del sensore appartenente al nodo sensore
-				    	jsonmsg.put("nameSensor", this.sensors.get(i).getName()); 
-				    	jsonmsg.put("type", this.sensors.get(i).getType()); 
-				    	jsonmsg.put("value", this.sensors.get(i).measurement());
-				    	jsonmsg.put("data", new Date());
-				    				    	
-					    System.out.println("MESSAGGIO INVIATO: " + jsonmsg);
-						CoapResponse resp=coap_client.request(CoapRequestMethod.POST, resource_uri, CoapResource.FORMAT_TEXT_PLAIN_UTF8, jsonmsg.toString().getBytes());
-							
-					//	resp = coap_client.request(CoapRequestMethod.POST, resource_uri, 0, "751".getBytes());
-						
-						
-						if (resp!=null) 
-							System.out.println("Response: "+resp);
-						else  
-							System.out.println("Request failure");
-						
-						
-					}
+		//AVVIO LA COMPONENTE CLIENT COAP DEL NODO SENSORE 
+		ClientCOAP client = new SensorNodeCOAP().new ClientCOAP(); //provare direttamente ClientCOAP client = new ClientCOAP();  
+		client.start();
 				
-					time1 = System.currentTimeMillis();
-			    }
-			
-			}
-			
-		//	coap_client.halt();
-
-		//	System.out.println("FINE DEL RUN");
+		//POSSO METTERE UN CONTROLLO PER SCEGLERE SE ESEGUIRE O MENO IL THREAD DEL SERVER
+		//AVVIO LA COMPONENTE SERVER COAP DEL NODO SENSORE 
+		ServerCOAP server = new SensorNodeCOAP().new ServerCOAP();
+		server.start();
 	
 	
-		
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SocketException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 		
 	}
+	
+	
+	
 	
 	
 	
